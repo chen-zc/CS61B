9f346cbc87174988a2a549d6c1a06e269bc67455
Zhichao <hzczc@qq.com>
2019-10-01 16:52:05 -0500

    finish lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -6,10 +6,50 @@ import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
+
+import javax.swing.text.Position;
+import
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    /**
+     * Computes the width of row i for a size s hexagon.
+     * @param s The size of the hex.
+     * @param i The row number where i = 0 is the bottom row.
+     * @return
+     */
+    public static int hexRowWidth(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2*s - 1 - effectiveI;
+        }
+        return s + 2 * effectiveI;
+    }
+
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return -effectiveI;
+    }
+
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+        for (int y = 0; y < 2*s; y += 1) {
+            int thisRowY = p.y + y;
+            int xRowStart = p.x + hexRowOffset(s, y);
+            Position RowStart = new Position(xRowStart, thisRowY);
+            int rowWidth = hexRowWidth(s, y);
+
+            for (int x = 0; x < rowWidth; x += 1) {
+                int xi = RowStart.x + x;
+                int yi = RowStart.y;
+                world[xi][yi] = TETile.colorVariant(t, 32, 32, 32, RANDOM);
+            }
+        }
+    }
 }
